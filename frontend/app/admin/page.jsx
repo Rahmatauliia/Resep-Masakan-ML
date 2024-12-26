11d8531a90e15e763341570e26853c20a1e2ec2f
Rahmatauliia <rahmad.auliaa2018@gmail.com>
2024-12-26 16:11:53 +0700

    update admin page.jsx

--- frontend/app/admin/page.jsx
+++ frontend/app/admin/page.jsx
@@ -1,59 +1,56 @@
 export default async function DashboardPage() {
   const fetchTotalResep = async () => {
-    'use server'
-    const response = await fetch('http://127.0.0.1:8000/api/resep')
+    const response = await fetch('http://127.0.0.1:8000/api/resep', {
+      cache: "no-store",
+    })
     const responseResepJson = await response.json();
-    console.log(responseResepJson)
-    // return responseResepJson?.data?.length
+    return responseResepJson?.data?.length
   }
   const fetchTotalUser = async () => {
-    'use server'
-    const response = await fetch('http://localhost:8000/api/total-user')
+    const response = await fetch('http://127.0.0.1:8000/api/total-user', {
+      cache: "no-store",
+    })
     const responseJson = await response.json();
-    console.log(responseJson)
-    // return responseJson?.total;
+    return responseJson?.total;
   }
   const fetchTotalDaerah = async () => {
-    'use server'
-    const response = await fetch('http://localhost:8000/api/daerah')
+    const response = await fetch('http://127.0.0.1:8000/api/daerah', {
+      cache: "no-store",
+    })
     const responseJson = await response.json();
-    console.log(responseJson)
-    // return responseJson?.data?.length;
+    return responseJson?.data?.length;
   }
   const fetchTotalKategori = async () => {
-    'use server'
-    const response = await fetch('http://localhost:8000/api/kategori')
+    const response = await fetch('http://127.0.0.1:8000/api/kategori', {
+      cache: "no-store",
+    })
     const responseJson = await response.json();
-    // return responseJson?.data?.length;
+    return responseJson?.data?.length;
   }
 
-  await fetchTotalDaerah()
-  await fetchTotalKategori()
-  await fetchTotalResep()
-  await fetchTotalUser()
-  // const totalResep = await fetchTotalResep()
-  // const totalUser = await fetchTotalUser()
-  // const totalDaerah = await fetchTotalDaerah()
-  // const totalKategori = await fetchTotalKategori()
+  const totalResep = await fetchTotalResep()
+  const totalUser = await fetchTotalUser()
+  const totalDaerah = await fetchTotalDaerah()
+  const totalKategori = await fetchTotalKategori()
   return (
     <div className="p-6">
       <h1 className="text-3xl font-semibold mb-4">Dashboard</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div className="bg-white rounded-lg shadow-md p-6">
           <h2 className="text-xl font-semibold mb-2">Total Users</h2>
-          {/* <p className="text-3xl font-bold">{totalUser}</p> */}
+          <p className="text-3xl font-bold">{totalUser}</p>
         </div>
         <div className="bg-white rounded-lg shadow-md p-6">
           <h2 className="text-xl font-semibold mb-2">Total Resep</h2>
-          {/* <p className="text-3xl font-bold">{totalResep}</p> */}
+          <p className="text-3xl font-bold">{totalResep}</p>
         </div>
         <div className="bg-white rounded-lg shadow-md p-6">
           <h2 className="text-xl font-semibold mb-2">Total Daerah</h2>
-          {/* <p className="text-3xl font-bold">{totalDaerah}</p> */}
+          <p className="text-3xl font-bold">{totalDaerah}</p>
         </div>
         <div className="bg-white rounded-lg shadow-md p-6">
           <h2 className="text-xl font-semibold mb-2">Total Kategori</h2>
-          {/* <p className="text-3xl font-bold">{totalKategori}</p> */}
+          <p className="text-3xl font-bold">{totalKategori}</p>
         </div>
       </div>
     </div>
